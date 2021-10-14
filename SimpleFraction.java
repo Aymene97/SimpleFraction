df787e0cda8e22d8666abad05dd90b5a3b144c15
uvsq22107773 <abd-elmoumen.drici@ens.uvsq.fr>
2021-10-14 19:05:50 +0200

    Creation constructeur

---
+++ SimpleFraction.java
@@ -0,0 +1,17 @@
+public class SimpleFraction {
+    SimpleFraction(int a){
+        System.out.println("hhhh");
+    }
+    SimpleFraction(){
+    }
+    String toStringg(){
+        return "SSSS";
+    }
+
+}
+class Main {
+    public static void main(String[] args) {
+        SimpleFraction obFR = new SimpleFraction(2);
+        System.out.println(obFR.toStringg());
+    }
+}
